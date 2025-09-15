62ee5cef3c4458d3415de49c475db07e405985bb
zouhelen <helenzou05@gmail.com>
2025-09-15 10:48:05 -0400

    method specs and status codes

--- main.go
+++ main.go
@@ -22,13 +22,18 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form to create a new snippet..."))
 }
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(201)
+	w.Write([]byte("Save a new snippet..."))
+}
 
 func main() {
 	mux := http.NewServeMux()
 	// Go treats '/' like a catch all
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("starting server on :4000")
 
